8bc29f37750dc98f0dc8aaa479ff0747e50a11bf
sandesh <sandeshghadage3@gmail.com>
2024-03-13 22:57:49 +0530

    commit

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
+import Step4 from "./Step4";
 
 export default function Form() {
   return (
@@ -10,7 +11,8 @@ export default function Form() {
         style={{ height: "100%" }}
       >
         {/* <Step1 /> */}
-        <Step2 />
+        {/* <Step2 /> */}
+        <Step4 />
       </div>
     </div>
   );
