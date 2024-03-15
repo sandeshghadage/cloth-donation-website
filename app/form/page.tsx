71bb53689af6a2c2aa317bf715460ce29a0f0d1c
Neha Rajbhar <rajrajbhar682@gmail.com>
2024-03-16 00:16:50 +0530

    step4

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,11 +1,13 @@
+"use client";
 import React, { useState } from "react";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step4 from "./Step4";
 
+
 export default function Form() {
   return (
-    <div className="border p-6 flex justify-center " style={{ height: "80vh" }}>
+    <div className="border p-6 flex justify-center " >
       <div
         className="bg-white container flex justify-center items-center flex-col rounded"
         style={{ height: "100%" }}
